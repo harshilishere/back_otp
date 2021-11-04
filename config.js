3308ac30f55e20acd0696c26d6bf0fbb8f4c49ac
harshilishere <61970592+harshilishere@users.noreply.github.com>
2021-11-04 14:43:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
 	MONGODB:'mongodb://localhost:27017/usb',
 	SECRET_KEY:'usbredirector',
-	SENDGRID_API_KEY:'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',
+	SENDGRID_API_KEY:'sendgrid key',
 	JWT_ACC:'regkey',
 	CLIENT_URL:'http://localhost:3300',
 	RESET_PASS:'accountresetpass',
